a60c1452cc65cfdf110489fe0f685111230954b0
James Jolley <james@jolley.co>
2025-12-01 14:33:16 +0000

    generalized solutions for 2025 day 1

--- source/solutions/year2025day01.cpp
+++ source/solutions/year2025day01.cpp
@@ -4,12 +4,15 @@
 
 #include "solution.hpp"
 
-namespace y2025d01 {
+namespace y2025d01
+{
 
-    std::vector<int> loadRotations(std::ifstream& in) {
+    std::vector<int> loadRotations(std::ifstream &in)
+    {
         std::vector<int> rotations;
         std::string rotation;
-        while(std::getline(in, rotation)) {
+        while (std::getline(in, rotation))
+        {
             int n = std::stoi(rotation.substr(1));
             if (rotation[0] == 'L')
                 n *= -1;
@@ -21,14 +24,43 @@ namespace y2025d01 {
     const int VALUES = 100;
     const int START = 50;
     const int MATCH = 0;
-    int part1(std::ifstream& in) {
+
+    int applyRotation(int rotation, int &value)
+    {
+        value = ((value + rotation) % VALUES);
+        if (value == MATCH)
+            return 1;
+        else
+            return 0;
+    }
+
+    int applyRotation434C49434B(int rotation, int &value)
+    {
+        if (rotation < 0 && value == 0)
+            value = VALUES;
+        int passes = 0;
+        value += rotation;
+        if (rotation >= 0)
+            passes = value / VALUES;
+        else if (value <= 0)
+            passes = (-value / VALUES) + 1;
+        value %= VALUES;
+        if (value < 0)
+            value += VALUES;
+        return passes;
+    }
+
+    int generalized(std::ifstream &in, advhb::PuzzlePart part)
+    {
         std::vector<int> rotations = loadRotations(in);
         int value = START;
         int matches = 0;
-        for (int rotation : rotations) {
-            value = ((value + rotation) % VALUES);
-            if (value == MATCH)
-                matches++;
+        for (int rotation : rotations)
+        {
+            if (part == advhb::PuzzlePart::PartOne)
+                matches += applyRotation(rotation, value);
+            else if (part == advhb::PuzzlePart::PartTwo)
+                matches += applyRotation434C49434B(rotation, value);
         }
         return matches;
     }
@@ -37,38 +69,13 @@ namespace y2025d01 {
         2025,
         1,
         advhb::PuzzlePart::PartOne,
-        [](std::ifstream& in){return std::vector<std::string>{std::to_string(part1(in))};}
+        [](std::ifstream &in){ return std::vector<std::string>{std::to_string(generalized(in,advhb::PuzzlePart::PartOne))}; }
     );
-
-    int part2(std::ifstream& in) {
-        std::vector<int> rotations = loadRotations(in);
-        int value = START;
-        int matches = 0;
-        for (int rotation : rotations) {
-            if (rotation < 0 && value == 0)
-                value = VALUES;
-            int passes = 0;
-            value += rotation;
-            if (rotation >= 0)
-                passes = value / VALUES;
-            else if (value <= 0)
-                passes = (-value / VALUES) + 1;            
-            matches += passes;
-            value %= VALUES;
-            if (value < 0)
-                value += VALUES;
-        }
-        return matches;
-    }
 
     advhb::Solution s2(
         2025,
         1,
         advhb::PuzzlePart::PartTwo,
-        [](std::ifstream& in){return std::vector<std::string>{std::to_string(part2(in))};}
+        [](std::ifstream &in){ return std::vector<std::string>{std::to_string(generalized(in,advhb::PuzzlePart::PartTwo))}; }
     );
-
-
-
-
 }
