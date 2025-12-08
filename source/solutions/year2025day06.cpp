6e848dd10f72b76ffb99487c6bacc93582d1ee82
James Jolley <james@jolley.co>
2025-12-08 12:25:50 +0000

    2025 day 6 complete

--- source/solutions/year2025day06.cpp
+++ source/solutions/year2025day06.cpp
@@ -5,13 +5,6 @@
 #include <string>
 #include <tuple>
 
-#include <iostream>
-
-#include <stdexcept>
-#include <algorithm>
-#include <list>
-#include <iterator>
-
 #include "solution.hpp"
 
 namespace y2025d06
@@ -50,8 +43,36 @@ namespace y2025d06
         return {columns, ops};
     }
 
-    std::int64_t part1(std::ifstream &in) {
-        Homework hw = loadHomework(in);
+    Homework loadHomework2(std::ifstream &in) {
+        std::vector<std::string> lines;
+        std::string s;
+        std::vector<std::vector<std::int64_t>> columns;
+        std::vector<std::string> ops;
+        while (std::getline(in, s))
+            lines.push_back(s);
+        std::vector<std::int64_t> columnGroup;
+        for (std::size_t n = lines[0].size(); n > 0; n--) {
+            s = "";
+            for (std::size_t j = 0; j < lines.size(); j++) {
+                s += lines[j][n-1];
+            }
+            if (s.find_first_not_of(' ') != std::string::npos)
+                columnGroup.push_back(std::stoll(s));
+            if (s[s.size()-1] == '+' || s[s.size()-1] == '*') {
+                ops.push_back(s.substr(s.size()-1));
+                columns.push_back(columnGroup);
+                columnGroup.clear();
+            }
+        }
+        return {columns, ops};
+    }
+
+    std::int64_t generalized(std::ifstream &in, advhb::PuzzlePart part) {
+        Homework hw;
+        if (part == advhb::PuzzlePart::PartOne)
+            hw = loadHomework(in);
+        else
+            hw = loadHomework2(in);
         auto columns = std::get<0>(hw);
         auto ops = std::get<1>(hw);
         std::int64_t total = 0;
@@ -74,7 +95,13 @@ namespace y2025d06
         2025,
         6,
         advhb::PuzzlePart::PartOne,
-        [](std::ifstream &in){ return std::vector<std::string>{std::to_string(part1(in))}; }
+        [](std::ifstream &in){ return std::vector<std::string>{std::to_string(generalized(in, advhb::PuzzlePart::PartOne))}; }
     );
 
+    advhb::Solution s2(
+        2025,
+        6,
+        advhb::PuzzlePart::PartTwo,
+        [](std::ifstream &in){ return std::vector<std::string>{std::to_string(generalized(in, advhb::PuzzlePart::PartTwo))}; }
+    );
 }
